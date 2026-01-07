28a6ee3fbe5fd79020be7808ba91beb6ffc03dcc
LWEENDO123 <57754490+LWEENDO123@users.noreply.github.com>
2026-01-07 10:18:51 -0800

    Update security.py

--- CUZ/Notification/security.py
+++ CUZ/Notification/security.py
@@ -1,6 +1,6 @@
 # Notification/security.py
 from fastapi import Depends, HTTPException, status
-from USERS.security import get_current_user  # Base JWT from USERS
+from CUZ.USERS.security import get_current_user  # Base JWT from USERS
 
 async def get_current_student(current_user: dict = Depends(get_current_user)):
     """Public access for any student (no premium check)."""
